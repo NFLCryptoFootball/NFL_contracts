1b39f0b9406c280ed91be066e16b0eb2190865fb
Clement Lakhal <clementlakhal@gmail.com>
2018-12-09 01:21:16 +0100

    Added name, symbol and token URI tests

--- test/NFL.test.js
+++ test/NFL.test.js
@@ -16,6 +16,16 @@ contract('NFL', (accounts) => {
       instance = contractInstance;
     }));
 
+  it('Should check the name of the token', () => instance.name()
+    .then((name) => {
+      assert.equal(name, 'NFL Crypto Football', 'Name is wrong');
+    }));
+
+  it('Should check the symbol of the token', () => instance.symbol()
+    .then((symbol) => {
+      assert.equal(symbol, 'NFLC', 'Symbol is wrong');
+    }));
+
   it('Should create a new card and add it to account 1 balance', () => instance.createCard(
     testCard,
     accounts[1],
@@ -30,5 +40,10 @@ contract('NFL', (accounts) => {
     .then((card) => {
       assert.equal(card.name, testCard.name, 'Card name is wrong');
       assert.equal(card.birthdate, testCard.birthdate, 'Card birthdate is wrong');
+    }));
+
+  it('Should check the URI of card 0', () => instance.tokenURI(0)
+    .then((tokenURI) => {
+      assert.equal(tokenURI, 'https://nflcrypto.netlify.com/', 'Token URI is wrong');
     }));
 });
