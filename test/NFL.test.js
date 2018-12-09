8244e225e0b8ffd50d0814fdea570393e72bfbfe
Clement Lakhal <clementlakhal@gmail.com>
2018-12-09 01:15:59 +0100

    Added getCard() test

--- test/NFL.test.js
+++ test/NFL.test.js
@@ -5,6 +5,11 @@ const NFL = artifacts.require('NFL');
 
 let instance;
 
+const testCard = {
+  name: 'John Doe',
+  birthdate: Math.round(Date.now() / 1000),
+};
+
 contract('NFL', (accounts) => {
   it('Should deploy an instance of the NFL contract', () => NFL.deployed()
     .then((contractInstance) => {
@@ -12,15 +17,18 @@ contract('NFL', (accounts) => {
     }));
 
   it('Should create a new card and add it to account 1 balance', () => instance.createCard(
-    {
-      name: 'John Doe',
-      birthdate: Math.round(Date.now() / 1000),
-    },
+    testCard,
     accounts[1],
   ));
 
   it('Should check account 1 balance', () => instance.balanceOf(accounts[1])
     .then((balance) => {
       assert.equal(balance, 1, 'Balance is wrong');
+    }));
+
+  it('Should check card 0', () => instance.getCard(0)
+    .then((card) => {
+      assert.equal(card.name, testCard.name, 'Card name is wrong');
+      assert.equal(card.birthdate, testCard.birthdate, 'Card birthdate is wrong');
     }));
 });
