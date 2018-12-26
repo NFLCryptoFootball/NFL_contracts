4ab9d7ecec83bc72bc3ac5c9ad949fb4f2f43088
Clement Lakhal <clementlakhal@gmail.com>
2018-12-26 03:53:27 +0100

    Updated migrations script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,13 @@
 /* global artifacts */
 
 const NFLBase = artifacts.require('NFLBase');
+const NFLMarketplace = artifacts.require('NFLMarketplace');
+const TestToken = artifacts.require('TestToken');
 
 function deployContracts(deployer) {
   deployer.deploy(NFLBase);
+  deployer.deploy(NFLMarketplace);
+  deployer.deploy(TestToken);
 }
 
 module.exports = deployContracts;
