7c2a59fdd22de290441616e647965f7521e0a8fa
Clement Lakhal <clementlakhal@gmail.com>
2018-12-26 03:52:30 +0100

    Updated to Solidity 0.4.25 Created Sale struct

--- contracts/NFLMarketplace.sol
+++ contracts/NFLMarketplace.sol
@@ -1,11 +1,18 @@
-pragma solidity 0.4.24;
+pragma solidity 0.4.25;
 
-import "./NFLBase.sol"
+import "./NFLBase.sol";
 
 
 /**
  * @title NFLMarketplace
  */
 contract NFLMarketplace is NFLBase {
+    enum SaleType { Instant, Auction, Mixed }
 
+    struct Sale {
+        SaleType saleType;
+        uint256 startingAmount;
+        uint256 fixedAmount;
+        bytes32 currency;
+    }
 }
